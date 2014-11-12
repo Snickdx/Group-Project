d0846b1d6357a58c2ea479753b2106f56ecf665e
Snickdx <snickdx@gmail.com>
2014-11-11 22:00:14 -0400

    Created The Component Java Files

--- project/FileExplorer.java
+++ project/FileExplorer.java
@@ -1,10 +1,10 @@
 package project;
 
-public class test {
+public class FileExplorer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+
 	}
 
 }
