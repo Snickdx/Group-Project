9633825e1c780dd35914eb73dcf9ed84109ccd43
IzzyRahaman <izzyrahaman@gmail.com>
2014-11-16 20:20:47 -0400

    Added code to handle DELE and STOR commands on client

--- FTPClient/src/clientutils/Client.java
+++ FTPClient/src/clientutils/Client.java
@@ -1,6 +1,9 @@
 package clientutils;
 
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.*;
 import java.util.*;
 
@@ -35,6 +38,11 @@ public abstract class Client
 		toServer.writeBytes(str + "\r\n");
 	}
 	
+	private void writeToServer(byte[] bytes) throws IOException
+	{
+		toServer.write(bytes, 0, bytes.length);
+	}
+	
 	private String readServer() throws IOException
 	{
 		return fromServer.readLine();
@@ -61,6 +69,43 @@ public abstract class Client
 		return new ServerResp<String[]>(arr, stat);
 	}
 	
+	public ServerResp<String> rename(String oldFilename, String newFilename) 
+			throws IOException, PoorlyFormedFTPResponse, InvalidFTPCodeException
+	{
+		writeToServer("RNFR " + oldFilename);
+		writeToServer("RNTO " + newFilename);
+		String resp = readServer();
+		FTPParseProduct prod = this.respParser.parseResponse(resp);
+		Status stat = fact.getStatus(prod.getCode());
+		return new ServerResp<String>(prod.getBody(), stat);
+	}
+	
+	public ServerResp<String> delete(String filename)
+		throws IOException, PoorlyFormedFTPResponse, InvalidFTPCodeException
+	{
+		writeToServer("DELE " + filename);
+		String resp = readServer();
+		FTPParseProduct prod = this.respParser.parseResponse(resp);
+		Status stat = fact.getStatus(prod.getCode());
+		return new ServerResp<String>(prod.getBody(), stat);
+	}
+	
+	public ServerResp<String> stor(String dir, String filename) throws IOException, PoorlyFormedFTPResponse, InvalidFTPCodeException
+	{
+		File file = new File(dir + "\\" + filename);
+		FileInputStream stream = new FileInputStream(file);
+		long size = stream.getChannel().size();
+		writeToServer("STOR " + filename + " " + size);
+		Path path = Paths.get(dir + "\\" + filename);
+		byte[] arr = Files.readAllBytes(path);
+		writeToServer(arr);
+		String resp = readServer();
+		FTPParseProduct prod = this.respParser.parseResponse(resp);
+		Status stat = fact.getStatus(prod.getCode());
+		return new ServerResp<String>(prod.getBody(), stat);
+		
+		
+	}
 	
 	
 }
